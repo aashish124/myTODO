196de4e844f04063f85f6b4109957c4cf8054a29
aashish124 <aashishahlawat123@gmail.com>
2024-12-19 00:09:19 +0530

    Initial Commit

---
+++ MyTodo/MyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoApp.swift
+//  MyTodo
+//
+//  Created by Aashish Ahlawat on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
